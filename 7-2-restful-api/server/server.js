3e92a482d38b793fa63408a82da02043a07d2818
xfas51 <fatays1425@gmail.com>
2026-04-15 12:09:53 +0300

    TODO 4

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -30,3 +30,14 @@ app.post("/api/songs", async (req, res) => {
     res.status(400).json({ message: err.message || "Bad Request" });
   }
 });
+
+app.get("/api/songs", async (req, res) => {
+  const rows = await Song.find().sort({ createdAt: -1 });
+  res.json(rows);
+});
+
+app.get("/api/songs/:id", async (req, res) => {
+  const s = await Song.findById(req.params.id);
+  if (!s) return res.status(404).json({ message: "Song not found" });
+  res.json(s);
+});
